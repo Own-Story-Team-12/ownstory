529ed68f9b8fe1515fc4562e6c47024f9ec89823
choitaeyang <imsun0101@naver.com>
2023-06-09 16:00:40 +0900

    'release추가'

--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -16,7 +16,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 
 
-
+# check
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
