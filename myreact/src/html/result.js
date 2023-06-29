d4889f56771faee10fd63ea92390c917320f806a
KimJinSang <rkdfursnsqlc@gmail.com>
2023-06-29 16:44:23 +0900

    동화저장하기위해보내기

--- myreact/src/html/result.js
+++ myreact/src/html/result.js
@@ -19,14 +19,14 @@ function Body() {
     })
 
     api.post("http://127.0.0.1:8000/Save/", {
-      image : response.data.image,
-      title : response.data.title,
-      content : response.data.content,
-      ko_title : response.data.ko_tilte,
-      ko_content : response.data.ko_content,
-      TTS_example : response.data.TTS_example,
-      TTS_myvoice : response.data.TTS_myvoice
-    })
+      user: localStorage.getItem('IDinfo').slice(1,-1),
+       ...response.data
+      }
+      ,{
+        headers:{
+          'Content-Type':'multipart/form-data',
+        },
+      })
     .then(function (response) {
       console.log("good")
     })
