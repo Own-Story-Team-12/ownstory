74c8b89af3ca002a0c36a38b04382c4b0bf12781
Hyunggul <hyunggul94@gmail.com>
2023-06-29 17:42:30 +0900

    db 저장 확인

--- conf/Ai/views.py
+++ conf/Ai/views.py
@@ -11,45 +11,53 @@ from .models import Result
 from django.utils import timezone
 import json
 from Page.models import User
+from django.http import HttpResponse
 
+class SaveAPIView(APIView):
+    def post(self, request):
+        if request.method == 'POST':
+            print('시작')
+        #     json_data = request.POST.get('json_data')  # POST 요청에서 JSON 데이터 추출
 
-def save_json_data(request):
-    if request.method == 'POST':
-    #     json_data = request.POST.get('json_data')  # POST 요청에서 JSON 데이터 추출
+            # data = json.loads(request)
+            username = request.data.get('user')
+            user = User.objects.get(username = username)
 
-        data = json.loads(request.body)
-        id = data.get('id')
-        user = User.objects.get(id=id)
+            # 필드 값 추출
+           
+            image = request.data.get('image')
+            title = request.data.get('title')
+            content = request.data.get('content')
+            ko_title = request.data.get('ko_title')
+            ko_content = request.data.get('ko_content')
+            TTS_example = request.data.get('TTS_example')
+            TTS_myvoice = request.data.get('TTS_myvoice')
 
-        # 필드 값 추출
-        
-        image = data.get('image')
-        title = data.get('title')
-        content = data.get('content')
-        ko_title = data.get('ko_title')
-        ko_content = data.get('ko_content')
-        TTS_example = data.get('TTS_example')
-        TTS_myvoice = data.get('TTS_myvoice')
+            if image == "no":
+                image = "/media/books.png"
 
-        if image == "no":
-            image = "/media/books.png"
+            # 모델 인스턴스 생성 및 저장
+            result = Result()
+            result.image = image
+            result.title = title
+            result.content = content
+            result.ko_title = ko_title
+            result.ko_content = ko_content
+            result.audio_example = TTS_example
+            result.audio_myvoice = TTS_myvoice
+            result.user = user
+            result.pub_date = timezone.datetime.now()
 
-        # 모델 인스턴스 생성 및 저장
-        result = Result()
-        result.image = image
-        result.title = title
-        result.content = content
-        result.ko_title = ko_title
-        result.ko_content = ko_content
-        result.audio_example = TTS_example
-        result.audio_myvoice = TTS_myvoice
-        result.user = user
-        result.pub_date = timezone.datetime.now()
+            result.save()
+            print('저장')
+                
+            # response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+            # response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+            # response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+            # response.content = 'Data saved successfully.'
 
-        result.save()
-
-        return JsonResponse({'message': 'Data saved successfully.'})
-    return JsonResponse({'message': 'Invalid request.'}, status=400)
+            return JsonResponse({'message': 'Data saved successfully.'})
+        return JsonResponse({'message': 'Invalid request.'}, status=400)
 
 
 class ResultAPIView(APIView):
@@ -57,13 +65,11 @@ class ResultAPIView(APIView):
         #POST로 들어오고 img_file이라는 필드명을 get할 수 있으면
         if request.method == 'POST' and request.FILES.get('img_file'):
             file = request.FILES['img_file']
-            print(123123123123213123123)
             #media 폴더에 이미지 파일 저장 
             fs = FileSystemStorage(location=settings.MEDIA_ROOT)
             filename = fs.save(file.name, file)
             uploaded_file_url = fs.url(filename)
             
-            #
             response_data = result_image(uploaded_file_url)
 
             return Response(response_data, status=status.HTTP_200_OK)
