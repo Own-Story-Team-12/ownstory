e7cd191b9989b6fd515903e40e78f7bc1982365f
choitaeyang <imsun0101@naver.com>
2023-07-03 03:21:57 +0900

    'djangoviewupdate'

--- conf/Ai/views.py
+++ conf/Ai/views.py
@@ -2,62 +2,9 @@ from django.conf import settings
 from .serializers import ResultSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from django.core.files.storage import FileSystemStorage
 from Ai.module.returnresult import result_image, result_keyword
-from django.http import JsonResponse
-from .models import Result
-from django.utils import timezone
-import json
-from Page.models import User
-from django.http import HttpResponse
-
-class SaveAPIView(APIView):
-    def post(self, request):
-        if request.method == 'POST':
-            print('시작')
-        #     json_data = request.POST.get('json_data')  # POST 요청에서 JSON 데이터 추출
-
-            # data = json.loads(request)
-            username = request.data.get('user')
-            user = User.objects.get(username = username)
-
-            # 필드 값 추출
-           
-            image = request.data.get('image')
-            title = request.data.get('title')
-            content = request.data.get('content')
-            ko_title = request.data.get('ko_title')
-            ko_content = request.data.get('ko_content')
-            TTS_example = request.data.get('TTS_example')
-            TTS_myvoice = request.data.get('TTS_myvoice')
-
-            if image == "no":
-                image = "/media/books.png"
-
-            # 모델 인스턴스 생성 및 저장
-            result = Result()
-            result.image = image
-            result.title = title
-            result.content = content
-            result.ko_title = ko_title
-            result.ko_content = ko_content
-            result.audio_example = TTS_example
-            result.audio_myvoice = TTS_myvoice
-            result.user = user
-            result.pub_date = timezone.datetime.now()
-
-            result.save()
-            print('저장')
-                
-            # response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-            # response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
-            # response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-            # response.content = 'Data saved successfully.'
-
-            return JsonResponse({'message': 'Data saved successfully.'})
-        return JsonResponse({'message': 'Invalid request.'}, status=400)
 
 
 class ResultAPIView(APIView):
@@ -65,11 +12,13 @@ class ResultAPIView(APIView):
         #POST로 들어오고 img_file이라는 필드명을 get할 수 있으면
         if request.method == 'POST' and request.FILES.get('img_file'):
             file = request.FILES['img_file']
+            
             #media 폴더에 이미지 파일 저장 
             fs = FileSystemStorage(location=settings.MEDIA_ROOT)
             filename = fs.save(file.name, file)
             uploaded_file_url = fs.url(filename)
             
+            #
             response_data = result_image(uploaded_file_url)
 
             return Response(response_data, status=status.HTTP_200_OK)
@@ -79,18 +28,14 @@ class ResultAPIView(APIView):
             feature = request.data.get('feature')
             background = request.data.get('background')
             content = request.data.get('content')
-            print(1)
+      
             ko_keyword = [name, feature, background, content]
-            print(2)
+         
             # 응답 데이터 구성
             response_data = result_keyword(ko_keyword)
-            print(3)
+           
 
             return Response(response_data, status=status.HTTP_200_OK)
         
 #Django REST Framework는 기본적으로 JSON 시리얼라이저를 사용하므로,
 #Python의 dict 객체를 Response 객체에 전달하면 자동으로 JSON으로 시리얼라이즈
-
-
-# class SaveViewSet(ModelViewSet):
-#     pass
