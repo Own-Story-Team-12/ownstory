8dd36b0fbea0813504b7ab797c27414a3b1f02e9
KimJinSang <rkdfursnsqlc@gmail.com>
2023-07-03 11:58:20 +0900

    test

--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     "Ai",
     'corsheaders',
     "post",
+    'Voice'
 ]
 
 MIDDLEWARE = [
--- conf/conf/urls.py
+++ conf/conf/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('Upload/', include('Upload.urls')),
     path('Ai/', include('Ai.urls')),
     path('post/', include('post.urls')),
+    path('Voice/', include('Voice.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
